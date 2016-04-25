b71a992d30ac36a7dfdc9b0c394ea9cfff982c9e
tjcarskadon <tjcarskadon@gmail.com>
2016-04-25 16:23:12 -0700

    refactor to use bind in step function

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,25 +1,14 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
-
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-
   this.oldStep = this.step;
-
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
-makeBlinkyDancer.prototype.step = function(context) {
-  context = context || this;
-  makeDancer.prototype.step.call(this, context);
-  // call the old version of step at the beginning of any call to this new version of step
+makeBlinkyDancer.prototype.step = function() {
+  // context = context || this;
+  makeDancer.prototype.step.call(this);
   
-  // this.oldStep();
-  
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  context.$node.toggle();
+  this.$node.toggle();
 };
--- src/dancer.js
+++ src/dancer.js
@@ -1,25 +1,24 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
-  var context = this;
+  // var context = this;
   this.timeBetweenSteps = timeBetweenSteps;
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
 
-  debugger;
-  this.step(context);
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
+  this.step();
   this.setPosition(top, left);
 };
 
-makeDancer.prototype.step = function(context) {
+makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
   setTimeout(function() {
-    context.step(context);
-  }, this.timeBetweenSteps);
+    this.step();
+  }.bind(this), this.timeBetweenSteps);
 };
 
 makeDancer.prototype.setPosition = function(top, left) {
