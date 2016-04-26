4cee691e13767b12cacd60ff1ce3f4869b6d979e
tjcarskadon <tjcarskadon@gmail.com>
2016-04-26 12:30:12 -0700

    refactor boundry detection to super class

--- src/dancer.js
+++ src/dancer.js
@@ -7,6 +7,10 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
   this.vx = 0;
   this.vy = 0;
+  this.startFlag = true;
+  this.speed = 70;
+  this.driftCorrection = 0;
+
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.step();
@@ -16,9 +20,45 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
+  this.x = this.$node.offset().left;
+  this.y = this.$node.offset().top;
+  var classCheck = this.$node.attr('class');
+  console.log(this.class);
+
   setTimeout(function() {
     this.step();
   }.bind(this), this.timeBetweenSteps);
+
+  //boundry detectoin
+  if (this.x <= 0) {
+    //left boundry
+    this.vx = this.speed;
+    this.vy = 0;
+    classCheck === 'pacman' && this.$node.css({transform: 'rotate(0deg)'});
+  } else if (this.x >= $('body').width() - 50 && !this.startFlag) {
+    //right boundry
+    this.vx = this.speed * -1;
+    this.vy = 0;
+    classCheck === 'pacman' && this.$node.css({transform: 'rotate(180deg)'});
+  } else if (this.y <= 0) {
+    //top boundry
+    this.vx = this.driftCorrection;
+    this.vy = this.speed;
+    classCheck === 'pacman' && this.$node.css({transform: 'rotate(90deg)'});
+  } else if (this.y >= $('body').height() - 50) {
+    //bottom boundry
+    this.vx = this.driftCorrection;
+    this.vy = this.speed * -1;
+    tclassCheck === 'pacman' && his.$node.css({transform: 'rotate(-90deg)'});
+  }
+  console.log(this.vx);
+  console.log(this.vy);
+
+//constant motion 
+  this.$node.animate({
+    left: this.x + this.vx,
+    top: this.y + this.vy
+  }, 100);
 };
 
 makeDancer.prototype.setPosition = function(top, left) {
@@ -31,3 +71,4 @@ makeDancer.prototype.setPosition = function(top, left) {
   };
   this.$node.css(styleSettings);
 };
+
--- src/pacman.js
+++ src/pacman.js
@@ -5,8 +5,8 @@ var makePacman = function (top, left, timeBetweenSteps) {
   this.$node = $('<img class="pacman" src="img/pManWide.png">');
   this.setPosition(top, left);
   this.frame = 0;
-  this.startFlag = true;
-  this.speed = 70;
+  // this.startFlag = true;
+  // this.speed = 70;
   this.driftCorrection = 2.75;
 };
 
@@ -21,6 +21,7 @@ makePacman.prototype.step = function() {
   this.x = pacmanNode.offset().left;
   this.y = pacmanNode.offset().top;
   
+  //manage start position for pacman
   if (this.startFlag) {
     if (this.x < $('body').width() * .6) {
       this.vx = 0;
@@ -29,33 +30,35 @@ makePacman.prototype.step = function() {
     }
   }
   
-  if (this.x <= 0) {
-    //left boundry
-    this.vx = this.speed;
-    this.vy = 0;
-    pacmanNode.css({transform: 'rotate(0deg)'});
-  } else if (this.x >= $('body').width() - 50 && !this.startFlag) {
-    //right boundry
-    this.vx = this.speed * -1;
-    this.vy = 0;
-    this.$node.css({transform: 'rotate(180deg)'});
-  } else if (this.y <= 0) {
-    //top boundry
-    this.vx = this.driftCorrection;
-    this.vy = this.speed;
-    this.$node.css({transform: 'rotate(90deg)'});
-  } else if (this.y >= $('body').height() - 50) {
-    //bottom boundry
-    this.vx = this.driftCorrection;
-    this.vy = this.speed * -1;
-    this.$node.css({transform: 'rotate(-90deg)'});
-  }
-  console.log(this.vx);
-  console.log(this.vy);
+//   //boundry detectoin
+//   if (this.x <= 0) {
+//     //left boundry
+//     this.vx = this.speed;
+//     this.vy = 0;
+//     pacmanNode.css({transform: 'rotate(0deg)'});
+//   } else if (this.x >= $('body').width() - 50 && !this.startFlag) {
+//     //right boundry
+//     this.vx = this.speed * -1;
+//     this.vy = 0;
+//     this.$node.css({transform: 'rotate(180deg)'});
+//   } else if (this.y <= 0) {
+//     //top boundry
+//     this.vx = this.driftCorrection;
+//     this.vy = this.speed;
+//     this.$node.css({transform: 'rotate(90deg)'});
+//   } else if (this.y >= $('body').height() - 50) {
+//     //bottom boundry
+//     this.vx = this.driftCorrection;
+//     this.vy = this.speed * -1;
+//     this.$node.css({transform: 'rotate(-90deg)'});
+//   }
+//   console.log(this.vx);
+//   console.log(this.vy);
 
-  this.$node.animate({
-    left: this.x + this.vx,
-    top: this.y + this.vy
-  }, 100);
+// //constant motion 
+//   this.$node.animate({
+//     left: this.x + this.vx,
+//     top: this.y + this.vy
+//   }, 100);
 
 };
